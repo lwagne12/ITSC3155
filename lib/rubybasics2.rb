b0128d203feeec5ec09a23aa83e6c3250c40b440
Luke <gus.wagner99@gmail.com>
2019-09-15 16:49:15 +0000

    Ruby Basics 2 - Part 2

--- lib/rubybasics2.rb
+++ lib/rubybasics2.rb
@@ -14,6 +14,17 @@ end
 # Part II
 def starts_with_consonant? s
   # YOUR CODE HERE
+   if /[aeiouAEIOU]/.match(s[0])
+      return false
+    elsif /0-9/.match(s[0])
+      return false
+    elsif /[^a-zA-Z0-9.]/.match(s)
+      return false
+    elsif /^$/.match(s)
+      return false
+    else
+      return true
+   end
 end
 
 # Part III
