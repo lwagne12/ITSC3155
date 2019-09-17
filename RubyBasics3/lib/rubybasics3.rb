9c783d1dc4aa8c066af929ed01afcf90e68d0286
Luke <gus.wagner99@gmail.com>
2019-09-17 14:44:08 +0000

    Ruby 00 Basics - Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -2,4 +2,16 @@
 
 class BookInStock
 # YOUR CODE HERE
+    @isbn = 0
+    @price = 0.0
+    
+    attr_accessor :isbn, :price
+    
+    def initialize(isbn, price)
+        @isbn = isbn
+        @price = price
+    end
+    
+    
+    
 end
