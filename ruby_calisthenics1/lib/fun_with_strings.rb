6e3813d949b269a978bcba673e3410156aeb0900
Luke <gus.wagner99@gmail.com>
2019-09-20 03:00:16 +0000

    Lab 9: Fun With Words - count words

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -5,12 +5,20 @@ module FunWithStrings
   end
   def count_words
     # your code here
+    sentence = Hash.new
+    self.downcase.gsub(/[^a-z\s]/,'').split.each do |word|
+      if sentence[word] != nil
+        sentence[word] += 1
+      else
+        sentence[word] = 1
+      end
+    end
+      sentence
   end
   def self.anagram_groups
     # your code here
   end
 end
-
 # make all the above functions available as instance methods on Strings:
 
 class String
