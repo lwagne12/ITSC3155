c48e5bb28ccad3e557c52b011478cd5397ffb1f8
Luke <gus.wagner99@gmail.com>
2019-09-20 02:43:52 +0000

    Lab 9: Fun With Words - Palidrome

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -1,11 +1,12 @@
 module FunWithStrings
   def palindrome?
     # your code here
+    self.gsub(/\W/,'').downcase == self.gsub(/\W/,'').downcase.reverse
   end
   def count_words
     # your code here
   end
-  def anagram_groups
+  def self.anagram_groups
     # your code here
   end
 end
