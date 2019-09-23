49b21a90454c9bc2ea6db4622aeadefe3388f8da
Luke <gus.wagner99@gmail.com>
2019-09-23 04:07:24 +0000

    Lab 9: Fun With Words - anagram_groups

---
+++ lib/fun_with_strings.rb
@@ -0,0 +1,27 @@
+module FunWithStrings
+  def palindrome?
+    # your code here
+    self.gsub(/\W/,'').downcase == self.gsub(/\W/,'').downcase.reverse
+  end
+  def count_words
+    # your code here
+    sentence = Hash.new
+    self.downcase.gsub(/[^a-z\s]/,'').split.each do |word|
+      if sentence[word] != nil
+        sentence[word] += 1
+      else
+        sentence[word] = 1
+      end
+    end
+      sentence
+  end
+  def anagram_groups
+    # your code here
+    self.downcase.split.group_by{ |x| x.chars.sort }.values
+  end
+end
+# make all the above functions available as instance methods on Strings:
+
+class String
+  include FunWithStrings
+end
