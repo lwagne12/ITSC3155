94c355d32c44ca1803bcfb692ab401db53793e93
Luke <gus.wagner99@gmail.com>
2019-09-30 20:29:59 +0000

    Ruby Basics2 changes

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -14,9 +14,21 @@ end
 # Part II
 def starts_with_consonant? s
   # YOUR CODE HERE
+   if /[aeiouAEIOU]/.match(s[0])
+      return false
+    elsif /0-9/.match(s[0])
+      return false
+    elsif /[^a-zA-Z0-9.]/.match(s)
+      return false
+    elsif /^$/.match(s)
+      return false
+    else
+      return true
+   end
 end
 
 # Part III
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+ return true if s == "0"
+	/^[10]*00$/.match(s) != nil
 end
