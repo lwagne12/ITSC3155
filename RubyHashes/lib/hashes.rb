7de7eebd37598954e760addfd8df1504ec9c0ff8
Luke <gus.wagner99@gmail.com>
2019-09-30 20:30:23 +0000

    Ruby Hashes  changes

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -2,14 +2,60 @@
 # Part I
 def array_2_hash emails, contacts
     # YOUR CODE HERE
+    contacts2 = {}
+    x = 0
+    
+    if emails[0] == nil
+        return contacts
+    else
+        contacts.each do |key, value|
+            key2 = "#{key}"
+            contacts2[key2] = emails[x]
+            x += 1
+        end
+    end
+    return contacts2
 end
 
 # Part II
 def array2d_2_hash contact_info, contacts
     # YOUR CODE HERE
+    contacts2 = {}
+    x = 0
+    
+    if contact_info[0][0] == nil
+        return contacts
+    end
+    
+    
+    contacts.each do |key, value|
+        key2 = :"#{key}"
+        contacts2[key2] = {:email => contact_info[x][0], :phone => contact_info[x][1]}
+        
+        x+=1
+    end
+    return contacts2
 end
 
 # Part III
 def hash_2_array contacts
     # YOUR CODE HERE
+    finalArray = []
+    emailArray = []
+    phoneArray = []
+    nameArray = []
+    
+    
+    contacts.each do |key, value|
+        key2 = :"#{key}"
+        
+        emailArray << contacts[key2][:email]
+        phoneArray << contacts[key2][:phone]
+        nameArray << "#{key}"
+    end
+    finalArray << emailArray
+    finalArray << phoneArray
+    finalArray << nameArray
+    
+    return finalArray
 end
